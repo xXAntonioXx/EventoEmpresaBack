74149c7666a54f5828f99e12c0e9176fe7f286c2
Antonio Gonzalez <jose_antony11@hotmail.com>
2019-07-09 11:45:36 -0500

    routing de vistas listo

--- routes/views.js
+++ routes/views.js
@@ -2,10 +2,12 @@ const express = require('express');
 const path = require('path');
 const app = express.Router();
 
+app.use(express.static('public'));
+
 app.get('/',(req,res)=>{
     let ruta = path.join(__dirname+'/../views/index.html');
-    //res.sendFile(ruta);
-    res.render(ruta);
+    res.sendFile(ruta);
+    //res.render(ruta);
 });
 
 module.exports = app;
