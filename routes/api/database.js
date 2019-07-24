9555377417e66bfab7d3b34af6e494cf724c9bc1
Antonio Gonzalez <jose_antony11@hotmail.com>
2019-07-24 18:01:59 -0500

    ya genera hash y valida el formulario

--- routes/api/database.js
+++ routes/api/database.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const dbInstance = require('../../lib/database/connection');
+const bcrypt = require('bcrypt');
 const bodyParser = require('body-parser');
 const { check, validationResult } = require('express-validator');
 
@@ -9,13 +10,26 @@ app.use(bodyParser.urlencoded({extended:false}));
 app.use(bodyParser.json());
 
 app.post('/nuevoRegistro',[
-    check('Nombre').isAlpha('es-ES'),
+    check('Nombre').matches(/^[\sa-zA-ZñáéíóúÁÉÍÓÚüÜ]+$/),
     check('Correo').isEmail(),
     check('Edad').isNumeric(),
-    check('expediente').isNumeric(),
-    check('Dependecia').isAlphanumeric(),
+    check('Expediente').isNumeric(),
+    check('Dependencia').matches(/^[\s\da-zA-ZñáéíóúÁÉÍÓÚüÜ]+$/)
 ],(req,res)=>{
-    res.send("todo en orden");
+    let errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        console.log(errors.array())
+        res.send(errors.isEmpty())
+        //res.redirect('/');
+    }else{
+        console.log("todo en orden");
+        let encryptObject = `${req.body['Nombre']}|${req.body['Correo']}|${req.body['Dependencia']}`;
+        bcrypt.hash(encryptObject,5,(err,hash)=>{
+            if (err){console.log("Errir al generar hash")};
+            res.send(`tu hash para qr es ${hash}`);
+        });
+        //res.send(errors.isEmpty());
+    }
 });
 
 module.exports = app;
