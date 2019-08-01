33e2327d1f4e6c9de3a53fed9b3ce142941a6c40
Antonio Gonzalez <jose_antony11@hotmail.com>
2019-08-01 18:04:17 -0500

    buscar servicio de smtp util

--- mailtest.js
+++ mailtest.js
@@ -1,1 +1,26 @@
 //aquí vamos a hacer test con mailgun y nodemailer
+var nodemailer = require('nodemailer');
+
+var transporter = nodemailer.createTransport({
+    service:'Mailgun',
+    auth:{
+        user:'postmaster@sandbox27e8329dcd0c4b06890245a87185efd9.mailgun.org',
+        pass:'544405626719e92334846df6c368c00f-f877bd7a-18fa05ab'
+    }
+});
+
+var mailOptions = {
+    from: '	rocker4nt0n10@gmail.com',
+    to: 'jose_antony11@hotmail.com',
+    subject: 'Esto es una prueba desde Node.js',
+    text: 'Esto es una prueba de mi poder D:'
+  };
+
+transporter.sendMail(mailOptions,function(err,info){
+    if(err){
+        console.log("sucedio un error");
+        console.log(err);
+    }else{
+        console.log("revisa la bandeja de outlook");
+    }
+});
